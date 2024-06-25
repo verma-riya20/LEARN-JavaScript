7b6992b3608736417c4d7e03290f988865307336
verma20 <riya204verma@gmail.com>
2024-06-25 18:25:06 +0530

    variables

---
+++ variables.js
@@ -0,0 +1,12 @@
+const accountId= "Riya02"
+let email=" riya@gmail.come"
+var password= "1234"
+accountCity= "Lucknow"
+
+/*
+prefer not to use var as 
+it has issue with block scope and functional scope
+*/
+
+console.log(accountId)
+console.table([ accountId,email,password,accountCity])
