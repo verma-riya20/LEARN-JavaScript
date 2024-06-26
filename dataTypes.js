a1f33d815675d8fde0bf3551d430435c40f36b4e
verma20 <riya204verma@gmail.com>
2024-06-27 01:10:49 +0530

    conversiona and operations

--- dataTypes.js
+++ dataTypes.js
@@ -2,3 +2,13 @@
 
 // alert(3+4) -- we are using nodejs not browser
 
+//number --2 power 53
+//bigint
+//boolean--true/false
+//null- standalone value 
+//undefined
+//object 
+//string--""
+
+console.log(typeof null)  //object
+console.log(typeof undefined)  //undefined
